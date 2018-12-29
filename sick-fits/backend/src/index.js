847ec761a1a7ddc17225ac537b1ec69fb1522eb2
Sam Anderson <samel.2710@gmail.com>
2018-12-29 17:25:53 +0000

    Render user signup front end

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,17 +1,22 @@
-require('dotenv').config({ path: 'variables.env'});
-const createServer = require('./createServer');
-const db = require('./db');
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
-// TODO: Use express middleare (JWT) to handle cookies
+server.express.use(cookieParser());
+
 // TODO: Use express middleware to populate current user
 
-server.start({
+server.start(
+  {
     cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    },
-}, details => {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  details => {
     console.log(`Server is running on port http://localhost:${details.port}`);
-});
+  }
+);
