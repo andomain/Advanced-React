8bc0c1c34a7f1a086db7f5efbd4e47cc8043c6a0
Sam Anderson <samel.2710@gmail.com>
2018-12-30 13:53:21 +0000

    Add User component and example display

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,15 +1,35 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import NavStyles from './styles/NavStyles';
+import User from "./User";
+import NavStyles from "./styles/NavStyles";
 
 const Nav = () => (
-    <NavStyles>
-        <Link href="/items"><a>Shop</a></Link>
-        <Link href="/sell"><a>Sell</a></Link>
-        <Link href="/signup"><a>Signup</a></Link>
-        <Link href="/orders"><a>Orders</a></Link>
-        <Link href="/me"><a>Me</a></Link>
-    </NavStyles>
+  <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me);
+        if (me) {
+          return me.name;
+        }
+        return null;
+      }}
+    </User>
+    <Link href="/items">
+      <a>Shop</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Me</a>
+    </Link>
+  </NavStyles>
 );
 
 export default Nav;
